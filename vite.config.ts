e629a19d4e8df70e68f7eab401ad9ea7c7472ba2
Manikanda Prabhu RaviChandran <rmkp@hotmail.com>
2025-01-02 10:38:33 +0530

    updated ecobamboo site

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ecobamboo/', // Replace 'ecobamboo' with your repository name
+  base: '/bamboo-site/', // Replace 'ecobamboo' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
